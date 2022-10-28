10ab95befc8a0febf19dc1def2cc5eda1b5d0bd0
kouekam <luckevinkouekam@gmail.com>
2022-10-28 08:57:23 +0100

    adding question , answer and likes models

--- likes/models.py
+++ likes/models.py
@@ -1,3 +1,8 @@
 from django.db import models
-
+from stackQA.models import Question,Answer
+from users.models import User
 # Create your models here.
+
+class Like(models.Model):
+    liker = models.ForeignKey(User,related_name='liker', on_delete=models.CASCADE)
+    post = models.ForeignKey(Question,Answer)
