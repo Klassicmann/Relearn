ee6a0339fd5036619f68473184fb922bf3d17bec
duclairdeugoue <87701146+duclairdeugoue@users.noreply.github.com>
2022-10-29 18:39:00 +0100

    adding models for the users application

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Speciality, Classe, Subject, Person, StudentProfile, TeacherProfile
 
 # Register your models here.
+
+admin.site.register(Speciality)
+admin.site.register(Classe)
+admin.site.register(Subject)
+admin.site.register(Person)
+admin.site.register(StudentProfile)
+admin.site.register(TeacherProfile)
