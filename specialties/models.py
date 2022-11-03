2e81c6a073f46ff250a5488d5eeae912d6c4a24e
duclairdeugoue <87701146+duclairdeugoue@users.noreply.github.com>
2022-11-03 11:12:35 +0100

    [Fixed]: Updating person model structure

--- specialties/models.py
+++ specialties/models.py
@@ -4,12 +4,21 @@ from django.db import models
 class Specialty(models.Model):
     # django already set auto primary key
     specialty_name = models.CharField(max_length=200)
+    
+    def __str__(self):
+        return self.specialty_name
 
 
 class Classe(models.Model):
     class_name = models.CharField(max_length=200)
     specialty = models.ForeignKey(Specialty, on_delete=models.CASCADE)
 
+    def __str__(self):
+        return self.class_name + " " + self.specialty.specialty_name
 
 class Subject(models.Model):
     classe = models.ForeignKey(Classe, on_delete=models.CASCADE)
+    subject_name = models.CharField(max_length=200, default=True)
+    
+    def __str__(self):
+        return self.subject_name
--- users/models.py
+++ users/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 from django.contrib.auth.models import User
 
 from specialties.models import Classe
@@ -13,18 +14,41 @@ class Person(models.Model):
     current_address = models.CharField(max_length=100, null=True)
     image = models.ImageField(default='default.jpg', upload_to='profile_pics')
     email = models.CharField(max_length=200)
-    person_type = models.BooleanField(default=True)
-    status = models.BooleanField(default=True)
+    
+    class PersonType(models.TextChoices):
+        student = 'ST',_('Student')
+        teacher = 'TE',_('Teacher')
+
+    person_type = models.CharField(
+        max_length = 2,
+        choices = PersonType.choices,
+        default = PersonType.student
+    )
+
+    def __str__(self):
+        return self.first_name + ' ' + self.last_name
 
 
 class StudentProfile(models.Model):
     student = models.ForeignKey(Person, on_delete=models.CASCADE)
     classe = models.ForeignKey(Classe, on_delete=models.CASCADE)
+    
+    def __str__(self):
+        return str(
+            {
+                'Name ' : self.student.first_name + " " + self.student.last_name,
+                'Classe ' : self.classe.class_name,
+                'Specialty ': self.classe.specialty.specialty_name
+            }
+        )
 
 
 class TeacherProfile(models.Model):
     teacher = models.ForeignKey(Person, on_delete=models.CASCADE)
     bio = models.CharField(max_length=255, blank=True)
+    
+    def __str__(self):
+        return self.teacher.first_name + " " + self.teacher.last_name
 
 
 
